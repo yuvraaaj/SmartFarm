311db4bc8e106acea918f0fbce7359fdc50bc7df
Siddharth Sharma <siddharthsharma1808@gmail.com>
2021-06-15 23:38:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 from web import redirect
 
-from disease_dic import disease_dic
+
 from fertilizer_dic import fertilizer_dic
 import requests
 import json
@@ -22,51 +22,6 @@ from model import ResNet9
 
 # -------------------------LOADING THE TRAINED MODELS -----------------------------------------------
 
-# Loading plant disease classification model
-
-disease_classes = ['Apple___Apple_scab',
-                   'Apple___Black_rot',
-                   'Apple___Cedar_apple_rust',
-                   'Apple___healthy',
-                   'Blueberry___healthy',
-                   'Cherry_(including_sour)___Powdery_mildew',
-                   'Cherry_(including_sour)___healthy',
-                   'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot',
-                   'Corn_(maize)___Common_rust_',
-                   'Corn_(maize)___Northern_Leaf_Blight',
-                   'Corn_(maize)___healthy',
-                   'Grape___Black_rot',
-                   'Grape___Esca_(Black_Measles)',
-                   'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)',
-                   'Grape___healthy',
-                   'Orange___Haunglongbing_(Citrus_greening)',
-                   'Peach___Bacterial_spot',
-                   'Peach___healthy',
-                   'Pepper,_bell___Bacterial_spot',
-                   'Pepper,_bell___healthy',
-                   'Potato___Early_blight',
-                   'Potato___Late_blight',
-                   'Potato___healthy',
-                   'Raspberry___healthy',
-                   'Soybean___healthy',
-                   'Squash___Powdery_mildew',
-                   'Strawberry___Leaf_scorch',
-                   'Strawberry___healthy',
-                   'Tomato___Bacterial_spot',
-                   'Tomato___Early_blight',
-                   'Tomato___Late_blight',
-                   'Tomato___Leaf_Mold',
-                   'Tomato___Septoria_leaf_spot',
-                   'Tomato___Spider_mites Two-spotted_spider_mite',
-                   'Tomato___Target_Spot',
-                   'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
-                   'Tomato___Tomato_mosaic_virus',
-                   'Tomato___healthy']
-disease_model_path = 'Trained_Model/plant_disease_model.pth'
-disease_model = ResNet9(3, len(disease_classes))
-disease_model.load_state_dict(torch.load(
-    disease_model_path, map_location=torch.device('cpu')))
-disease_model.eval()
 
 
 # Loading crop recommendation model
@@ -104,27 +59,6 @@ def weather_fetch(city_name):
         return None
 
 
-def predict_image(img, model=disease_model):
-    """
-    Transforms image to tensor and predicts disease label
-    :params: image
-    :return: prediction (string)
-    """
-    transform = transforms.Compose([
-        transforms.Resize(256),
-        transforms.ToTensor(),
-    ])
-    image = Image.open(io.BytesIO(img))
-    img_t = transform(image)
-    img_u = torch.unsqueeze(img_t, 0)
-
-    # Get predictions from model
-    yb = model(img_u)
-    # Pick index with highest probability
-    _, preds = torch.max(yb, dim=1)
-    prediction = disease_classes[preds[0].item()]
-    # Retrieve the class label
-    return prediction
 
 # ===============================================================================================
 # ------------------------------------ FLASK APP -------------------------------------------------
@@ -157,12 +91,8 @@ def crop_checkup():
 def fertilizer_recommendation():
     return render_template('fertilizer.html')
 
-# render disease prediction input page
 
 
-@ app.route('/disease')
-def disease():
-    return render_template('disease.html')
 
 @ app.route('/weather')
 def weather_recommendation():
@@ -240,27 +170,6 @@ def fert_recommend():
     response = Markup(str(fertilizer_dic[key]))
 
     return render_template('fertilizer-result.html', recommendation=response)
-
-# render disease prediction result page
-
-
-@app.route('/disease-predict', methods=['GET', 'POST'])
-def disease_prediction():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            return redirect(request.url)
-        file = request.files.get('file')
-        if not file:
-            return
-        img = file.read()
-
-        prediction = predict_image(img)
-
-        prediction = Markup(str(disease_dic[prediction]))
-        return render_template('disease-result.html', prediction=prediction)
-
-    return render_template('disease.html')
-
 
 # ===============================================================================================
 if __name__ == '__main__':
